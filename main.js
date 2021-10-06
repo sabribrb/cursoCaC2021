b1f0115e5aac4357d69fd0ba668e814c8cd460a6
sabrina <89945956+sabribrb@users.noreply.github.com>
2021-10-06 14:37:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function desplegarNavIndex() {
+    var x = document.getElementById("index-nav");
+    if (x.className === "topnav" ) {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function desplegarNavPrograma() {
+
+    var x= document.getElementById("programa-nav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function desplegarNavInscribirse() {
+  
+    var x= document.getElementById("inscribirse-nav");
+    if (x.className === "topnav"  ) {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
